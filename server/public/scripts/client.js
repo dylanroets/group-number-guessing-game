de49cb27f60b2afb3442d2e62072bed18cce1a65
Dylan Roets <dylanroets@yahoo.com>
2022-11-03 11:54:18 -0500

    random number genertor communicating with client, button functionality tested

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,7 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  $('#submit-btn').on('click', submitGuesses)
   onRender (); 
 }
 
@@ -10,4 +11,17 @@ function onRender (){
     method: 'GET', 
     url: '/number-game'
   })
+  .then(function (response) {
+    console.log('in the onRender, response: ', response);
+    let randomNumberVariable = response;
+    console.log('changed response to randomNumberV:', randomNumberVariable);
+  })
+    .catch(function (error) {
+      alert('Number Load Failed', error)
+    });
 }
+
+function submitGuesses() {
+  console.log('in submitGuesses');
+}
+
--- server/server.js
+++ server/server.js
@@ -11,17 +11,19 @@ app.use(express.static('server/public'));
 
 // GET & POST Routes go here
 
-app.get('/number-game', () => { 
+app.get('/number-game', (req, res) => { 
   console.log("in server, render function"); 
   randomNumberFunction(1,10); 
-  console.log(randomNumberVariable); 
-})
+  res.send({randomNumberVariable});
+});
 
-app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
 
 let randomNumberVariable = 0; 
 function randomNumberFunction(min, max){ 
   randomNumberVariable = Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+
+app.listen(PORT, () => {
+  console.log ('Server is running on port', PORT)
+});
